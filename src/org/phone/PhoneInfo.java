4ab9002eaaa4cac33e27ed6a85929f2229b5c032
shalinieva <emailshalini17@gmail.com>
2023-02-22 08:19:30 +0530

    before cloning

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,35 @@
+package org.phone;
+
+public class PhoneInfo {
+private void phoneName() {
+	// TODO Auto-generated method stub
+System.out.println("Phone Name : Nokia 7");
+}
+private void phoneMieiNum() {
+	// TODO Auto-generated method stub
+System.out.println("PhoneMieiNum :111111111");
+}
+private void camera() {
+	// TODO Auto-generated method stub
+System.out.println("camera : v100.002.33");
+}
+private void storage() {
+	// TODO Auto-generated method stub
+System.out.println("Storage:64GB");
+}
+private void osName() {
+	// TODO Auto-generated method stub
+System.out.println("OS Name : Android 10");
+}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+PhoneInfo p=new PhoneInfo();
+p.phoneName();
+p.phoneMieiNum();
+p.camera();
+p.osName();
+p.storage();
+}
+}
+
+
